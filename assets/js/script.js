123068b8aa37e480238202516f3dcdb1792bed15
Ciprian Turcu <turcuciprian1@gmail.com>
2017-05-31 13:27:51 +0300

    added redirect on otherwise for the route provider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
 var ngCip = angular.module("ngCip", ["ngRoute"]);
 ngCip.config(function($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "templates/start.html"
-    })
-    .when("/documenter_cover", {
-      templateUrl : "templates/start.html"
-    })
-    .when("/html_structure", {
-      templateUrl : "templates/html_structure.html"
-    })
-    .when("/css_structure", {
-      templateUrl : "templates/css_structure.html"
-    })
-    .when("/javascript", {
-      templateUrl : "templates/javascript.html"
-    })
-    .when("/template_features", {
-      templateUrl : "templates/template_features.html"
-    })
-    .when("/total_files", {
-      templateUrl : "templates/total_files.html"
-    })
-    .when("/source_credits", {
-      templateUrl : "templates/source_credits.html"
-    })
-    .when("/support", {
-      templateUrl : "templates/support.html"
-    })
-    .otherwise({
-        template : "<h1>None</h1><p>Nothing has been selected</p>"
-    });
+        .when("/", {
+            templateUrl: "templates/start.html"
+        })
+        .when("/documenter_cover", {
+            templateUrl: "templates/start.html"
+        })
+        .when("/html_structure", {
+            templateUrl: "templates/html_structure.html"
+        })
+        .when("/css_structure", {
+            templateUrl: "templates/css_structure.html"
+        })
+        .when("/javascript", {
+            templateUrl: "templates/javascript.html"
+        })
+        .when("/template_features", {
+            templateUrl: "templates/template_features.html"
+        })
+        .when("/total_files", {
+            templateUrl: "templates/total_files.html"
+        })
+        .when("/source_credits", {
+            templateUrl: "templates/source_credits.html"
+        })
+        .when("/support", {
+            templateUrl: "templates/support.html"
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 });
